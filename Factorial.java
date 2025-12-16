9800b131a2e5d0ecce3e8707ad3f63131fcf9744
GOWTHAM292004 <gowthamin209@gmail.com>
2025-12-16 09:48:49 +0530

    Java Factorial

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=1;
+        for (int i=a;a>=1;i--){
+            b*=a;
+            a--;
+        }
+        System.out.println(b);
+    }
+}
