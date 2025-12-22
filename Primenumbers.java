5cd9758d47afa5692126bec2b74cdd0a46a03884
Gowtham M <gowthamin209@gmail.com>
2025-12-22 12:45:24 +0530

    Implement prime number finder in Primenumbers.java
    Added functionality to find and print prime numbers within a given range.

--- Primenumbers.java
+++ Primenumbers.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Primenumbers {
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        int first=sc.nextInt();
+        int last=sc.nextInt();
+        for (int i=first;i<=last;i++){
+            boolean prime=true;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    prime=false;
+                    break;
+                }
+            }
+            if(prime==true){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
 
