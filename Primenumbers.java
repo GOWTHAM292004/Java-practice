ab1882d6e956403e3479dfbbd1e2560d62c7aa09
Gowtham M <gowthamin209@gmail.com>
2025-12-22 12:34:56 +0530

    Update Primenumbers.java

--- Primenumbers.java
+++ Primenumbers.java
@@ -1,15 +1,1 @@
-import java.util.Scanner;
-public class Primenumbers {
-    public static void main (String args[]){
-        Scanner sc=new Scanner(System.in);
-        int input=sc.nextInt();
-        for(int i=2;i<=input;i++){
-            for(int j=1;j<i;j++){
-                if(j%i==0){
-                    System.out.print(i+",");
-                    break;
-                }
-            }
-        }
-    }
-}
+
